191c9d93965f9e1357b19c9200977ac729e02e80
Geoyeob Kim <gasbank@msn.com>
2019-05-13 14:50:24 +0900

    refs #454 노란 블럭 (부셔질 수 있는 블록 수정)

---
+++ Assets/Scripts/YellowBlock.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMProText = TMPro.TextMeshPro;
+
+public class YellowBlock : MonoBehaviour {
+    [SerializeField] int hp = 3;
+    [SerializeField] float lastHitTime = 0;
+    [SerializeField] float lastHitMinInterval = 0.5f;
+    [SerializeField] TMProText hpText = null;
+
+    void Awake() {
+        hpText.text = hp.ToString();
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        Debug.Log($"YellowBlock collision enter: relativeVelocity magnitude: {collision.relativeVelocity.magnitude}");
+        var player = collision.gameObject.GetComponentInParent<HotairBalloon>();
+        if (Time.time - lastHitTime > lastHitMinInterval && player != null && player.IsGameOver == false) {
+            lastHitTime = Time.time;
+            hp--;
+            hpText.text = hp.ToString();
+            player.AddExplosionForce((collision.transform.position - collision.contacts[0].point).normalized);
+            if (hp <= 0) {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
