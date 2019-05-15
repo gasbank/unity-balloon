e05703483a4deb964f26fb5b64847c5934c98301
Geoyeob Kim <gasbank@gmail.com>
2019-05-15 20:32:34 +0900

    SFSoftShadows 삭제

--- Assets/SFSoftShadows/SFSoftShadowDemos/SpaceSpritesAndPrefabs/Spinner.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Spinner : MonoBehaviour {
-
-	void Update () {
-		this.transform.rotation = Quaternion.AngleAxis(90.0f*Time.time, Vector3.forward);
-	}
-}
