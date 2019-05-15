f8bdb236d746cd3388a61e7e6d36f399329b801f
Geoyeob Kim <gasbank@gmail.com>
2019-05-15 21:58:35 +0900

    refs #478 플레이어 캐릭터(열기구) 멋있게 만들기

---
+++ Assets/Scripts/Gear.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gear : MonoBehaviour {
+    void Update() {
+        //transform.Rotate()
+        transform.Rotate(Vector3.forward * Time.deltaTime * 100);
+    }
+}
