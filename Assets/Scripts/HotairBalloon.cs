9bdfc62edcbd31e626e0f4c7e3a6862de096c47d
merk Choi <team.gghf@gmail.com>
2019-05-21 18:31:45 +0900

    밸런스 패치 1. 대시 소모시간이 1.5 빠르게 지나가게하였습니다.                     RemainOilAmount -= Time.deltaTime *1.5f* burnSpeed;
    2. 피버모드시 소모시간이 2배 빠르게 지나가게 하였습니다.
    
             // 피버 아이템을 가지고 있지 않을 때만 감소
             if (feverItemParticle.activeSelf == false) {
    -            FeverGauge -= Time.deltaTime * feverGaugeDecrement;
    +            FeverGauge -= Time.deltaTime *2* feverGaugeDecrement;
                 // 피버 게이지가 바닥 나면
                 if (FeverGauge <= 0) {
                     // 피버 모드 종료

--- Assets/Scripts/HotairBalloon.cs
+++ Assets/Scripts/HotairBalloon.cs
@@ -162,7 +162,7 @@ public class HotairBalloon : MonoBehaviour {
                 }
 
                 if (IsOilConsumed) {
-                    RemainOilAmount -= Time.deltaTime * burnSpeed;
+                    RemainOilAmount -= Time.deltaTime *1.5f* burnSpeed;
                 }
             } else if (handleSlider.Controlled) {
                 balloonRb.velocity = new Vector3(balloonRb.velocity.x, defaultVelocity, balloonRb.velocity.z);
@@ -243,7 +243,7 @@ public class HotairBalloon : MonoBehaviour {
 
         // 피버 아이템을 가지고 있지 않을 때만 감소
         if (feverItemParticle.activeSelf == false) {
-            FeverGauge -= Time.deltaTime * feverGaugeDecrement;
+            FeverGauge -= Time.deltaTime *2* feverGaugeDecrement;
             // 피버 게이지가 바닥 나면
             if (FeverGauge <= 0) {
                 // 피버 모드 종료
