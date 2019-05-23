211232271dcc852cdd6beab605158fb82a8bced1
Geoyeob Kim <gasbank@msn.com>
2019-05-23 19:57:37 +0900

    refs #502 바람 영역 효과 게이지(충전/방전) 기능 만들기

--- Assets/Scripts/WindRegion.cs
+++ Assets/Scripts/WindRegion.cs
@@ -4,31 +4,61 @@ using UnityEngine;
 
 public class WindRegion : MonoBehaviour {
     [SerializeField] float windPower = 1.0f;
+    [SerializeField] float maxCapacity = 0.0f;
+    [SerializeField] float capacity = 0.0f;
+    [SerializeField] float dissipationRate = 10.0f;
+    [SerializeField] float rechargeRate = 10.0f;
+    [SerializeField] Transform gaugeRed = null;
+    [SerializeField] Transform gaugeGreen = null;
+    [SerializeField] int enteredCount = 0;
 
     public Vector3 WindForce => windPower * transform.right;
 
     void OnTriggerEnter(Collider collider) {
         var player = collider.gameObject.GetComponentInParent<HotairBalloon>();
         if (player != null && player.IsGameOver == false) {
-            player.AddWindForce(this);
+            enteredCount++;
+            if (maxCapacity == 0 || capacity > 0) {
+                player.AddWindForce(this);
+            }
+        }
+    }
+
+    void OnTriggerStay(Collider collider) {
+        var player = collider.gameObject.GetComponentInParent<HotairBalloon>();
+        if (player != null && player.IsGameOver == false) {
+            if (maxCapacity != 0) {
+                capacity = Mathf.Clamp(capacity - dissipationRate * Time.fixedDeltaTime, 0, maxCapacity);
+                if (capacity <= 0) {
+                    player.RemoveWindForce(this);
+                }
+            }
         }
     }
 
     void OnTriggerExit(Collider collider) {
         var player = collider.gameObject.GetComponentInParent<HotairBalloon>();
         if (player != null && player.IsGameOver == false) {
+            enteredCount--;
             player.RemoveWindForce(this);
         }
     }
 
-    // void OnTriggerStay(Collider collider) {
-    //     var player = collider.gameObject.GetComponentInParent<HotairBalloon>();
-    //     if (player != null && player.IsGameOver == false) {
-    //         player 
-    //         if (collider.attachedRigidbody != null) {
-    //             Debug.Log("RIGHT!!!");
-    //             collider.attachedRigidbody.velocity += Vector3.right * windPower;
-    //         }
-    //     }
-    // }
+    void Update() {
+        if (gaugeRed != null) {
+            gaugeRed.gameObject.SetActive(maxCapacity != 0);
+        }
+        if (gaugeGreen != null) {
+            gaugeGreen.gameObject.SetActive(maxCapacity != 0);
+        }
+
+        if (maxCapacity != 0) {
+            if (enteredCount == 0) {
+                capacity = Mathf.Clamp(capacity + rechargeRate * Time.deltaTime, 0, maxCapacity);
+            }
+
+            var ratio = Mathf.Clamp(capacity / maxCapacity, 0, 1);
+            gaugeGreen.localScale = new Vector3(ratio * gaugeRed.localScale.x, gaugeGreen.localScale.y, gaugeGreen.localScale.z);
+        }
+    }
 }
