37a4a45e5aee7f1c332efff02445ef917693dd7d
Geoyeob Kim <gasbank@msn.com>
2019-05-23 16:31:04 +0900

    사소한 컴파일러 경고 수정

--- Assets/Scripts/WindEffect.cs
+++ Assets/Scripts/WindEffect.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class WindEffect : MonoBehaviour {
     [SerializeField] float speed = 100.0f;
-    [SerializeField] new Renderer renderer = null;
+    [SerializeField] Renderer windRenderer = null;
 
     void Awake() {
-        renderer.material = Instantiate(renderer.material);
+        windRenderer.material = Instantiate(windRenderer.material);
     }
 
     void Update() {
         transform.Rotate(Vector3.up * Time.deltaTime * speed);
-        renderer.material.mainTextureOffset += Vector2.up * Time.deltaTime * speed;
+        windRenderer.material.mainTextureOffset += Vector2.up * Time.deltaTime * speed;
     }
 }
