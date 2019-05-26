f64d2f59b0faf6ce75479d649e60bb99c54d7610
Geoyeob Kim <gasbank@msn.com>
2019-05-26 15:16:51 +0900

    게임 시작 전에도 스테이지 플레이 시간이 증가하는 버그 수정

--- Assets/Scripts/StageDuration.cs
+++ Assets/Scripts/StageDuration.cs
@@ -7,14 +7,17 @@ using UnityEngine;
 public class StageDuration : MonoBehaviour {
     [SerializeField] TextMeshProUGUI text = null;
     [SerializeField] HotairBalloon hotairBalloon = null;
+    [SerializeField] float stageElapsedTime = 0;
 
     void Awake() {
         hotairBalloon = GameObject.Find("Hotair Balloon").GetComponent<HotairBalloon>();
+        stageElapsedTime = 0;
     }
 
     void Update() {
-        if (hotairBalloon.IsGameOver == false && hotairBalloon.IsStageFinished == false) {
-            text.text = (DateTime.MinValue + TimeSpan.FromSeconds(Time.timeSinceLevelLoad)).ToString("mm:ss.fff");
+        if (hotairBalloon.IsGameOver == false && hotairBalloon.IsStageFinished == false && hotairBalloon.IsTitleVisible == false) {
+            stageElapsedTime += Time.deltaTime;
         }
+        text.text = (DateTime.MinValue + TimeSpan.FromSeconds(stageElapsedTime)).ToString("mm:ss.fff");
     }
 }
