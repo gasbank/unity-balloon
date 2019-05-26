a75604acc071b6f6aca3ad789f38a2641ebb6c03
Geoyeob Kim <gasbank@gmail.com>
2019-05-27 00:03:17 +0900

    실제 기기에서는 오른쪽 위 숨김 버튼 빠르게 10회 눌러야 스테이지 선택 화면 뜨도록 하기

--- Assets/Scripts/BackButton.cs
+++ Assets/Scripts/BackButton.cs
@@ -2,5 +2,21 @@
 using UnityEngine.SceneManagement;
 
 public class BackButton : MonoBehaviour {
-    public void GoToStageSelection() => SceneManager.LoadScene("Stage Selection");
+    [SerializeField] float lastTouchTime = 0;
+    [SerializeField] int repeatedTouchCount = 0;
+    public void GoToStageSelection() {
+        if (Application.isEditor) {
+            SceneManager.LoadScene("Stage Selection");
+        } else {
+            if (Time.time - lastTouchTime < 0.3f) {
+                repeatedTouchCount++;
+                if (repeatedTouchCount >= 10) {
+                    SceneManager.LoadScene("Stage Selection");
+                }
+            } else {
+                repeatedTouchCount = 0;
+            }
+            lastTouchTime = Time.time;
+        }
+    }
 }
