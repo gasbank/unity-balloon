6278927cf60f6d5cbe241e7df8c3e999daed7ae6
Geoyeob Kim <gasbank@msn.com>
2019-05-27 14:00:09 +0900

    iOS 빌드 준비중

---
+++ build-appstore-ipa.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#security unlock-keychain -p ${!KEYCHAIN_PASSWORD}
+
+cd build
+
+xcodebuild \
+    -project Unity-iPhone.xcodeproj \
+    -scheme Unity-iPhone \
+    archive \
+    -archivePath build \
+    CODE_SIGN_STYLE="Manual" \
+    PROVISIONING_PROFILE_SPECIFIER="Balloon AppStore" \
+    CODE_SIGN_IDENTITY="iPhone Distribution: GEOYEOB KIM (TG9MHV97AH)"
+
+xcodebuild \
+    -exportArchive \
+    -exportOptionsPlist ../exportoptions-appstore.plist \
+    -archivePath "build.xcarchive" \
+    -exportPath "build"
+
