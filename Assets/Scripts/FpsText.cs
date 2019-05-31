e063691cebe8acf2a6ff102a207bc1f420b701c2
Geoyeob Kim <gasbank@gmail.com>
2019-05-31 14:00:09 +0900

    FPS 표시

---
+++ Assets/Scripts/FpsText.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class FpsText : MonoBehaviour {
+    [SerializeField] TextMeshProUGUI text = null;
+
+    void Update() {
+        text.text = (1.0f / Time.unscaledDeltaTime).ToString("f1");
+    }
+}
