cccc38e30d6b7f363296297942e5f09bdf1658b9
Geoyeob Kim <gasbank@msn.com>
2019-06-06 20:04:36 +0900

    빌드 오류 수정 (NativeLocale 누락)

---
+++ Assets/NativeLocale/Editor/NativeLocale.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+#if UNITY_IOS
+using ChillyRoom.UnityEditor.iOS.Xcode;
+#endif
+
+public class NativeLocale
+{
+ #if UNITY_IOS
+    public static void AddLocalizedStringsIOS(string projectPath, string localizedDirectoryPath)
+    {        
+        DirectoryInfo dir = new DirectoryInfo(localizedDirectoryPath);
+        if(!dir.Exists)
+            return;
+
+        List<string> locales = new List<string>();
+        var localeDirs = dir.GetDirectories("*.lproj", SearchOption.TopDirectoryOnly);
+
+        foreach(var sub in localeDirs)
+            locales.Add(Path.GetFileNameWithoutExtension(sub.Name));
+
+        AddLocalizedStringsIOS(projectPath, localizedDirectoryPath, locales);
+    }
+
+    public static void AddLocalizedStringsIOS(string projectPath, string localizedDirectoryPath, List<string> validLocales)
+    {
+        string projPath = projectPath + "/Unity-iPhone.xcodeproj/project.pbxproj";
+        PBXProject proj = new PBXProject();
+        proj.ReadFromFile(projPath);
+
+        foreach(var locale in validLocales)
+        {
+            // copy contents in the localization directory to project directory
+            string src = Path.Combine(localizedDirectoryPath, locale + ".lproj");
+            DirectoryCopy(src, Path.Combine(projectPath, "Unity-iPhone/" + locale + ".lproj"));
+
+            string fileRelatvePath = string.Format("Unity-iPhone/{0}.lproj/InfoPlist.strings", locale);
+            proj.AddLocalization("InfoPlist.strings", locale, fileRelatvePath);
+        }
+
+        proj.WriteToFile(projPath);
+    }
+
+
+    private static void DirectoryCopy(string sourceDirName, string destDirName)
+    {
+        DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+        if (!dir.Exists)
+            return;
+
+        if (!Directory.Exists(destDirName))
+        {
+            Directory.CreateDirectory(destDirName);
+        }
+
+        FileInfo[] files = dir.GetFiles();
+
+        foreach (FileInfo file in files)
+        {
+            // skip unity meta files
+            if(file.FullName.EndsWith(".meta"))
+                continue;
+            string temppath = Path.Combine(destDirName, file.Name);
+            file.CopyTo(temppath, true);
+        }
+
+        DirectoryInfo[] dirs = dir.GetDirectories();
+        foreach (DirectoryInfo subdir in dirs)
+        {
+            string temppath = Path.Combine(destDirName, subdir.Name);
+            DirectoryCopy(subdir.FullName, temppath);
+        }
+    }
+    #endif
+}
