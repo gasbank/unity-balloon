1f3fe37f65c802cdea1d7c3d3a817cb5a1fa10e9
Geoyeob Kim <gasbank@msn.com>
2019-06-11 15:42:59 +0900

    refs #536 스테이지 종료 처리 (광고 연동) - 25%, 50%, 75% 지점 체크포인트 표기

---
+++ Assets/Scripts/CheckpointLine.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Text3D = TMPro.TextMeshPro;
+
+public class CheckpointLine : MonoBehaviour {
+    [SerializeField] Text3D checkpointLineText = null;
+
+    public string CheckpointText {
+        get => checkpointLineText.text;
+        set => checkpointLineText.text = value;
+    }
+}
--- Assets/Scripts/Stage.cs
+++ Assets/Scripts/Stage.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class Stage : MonoBehaviour {
     [SerializeField] Material backgroundMaterial = null;
     [SerializeField] bool tutorial = false;
+    [SerializeField] GameObject checkpointPrefab = null;
+    [SerializeField] FinishLine finishLine = null;
 
     public bool Tutorial => tutorial;
 
     void Awake() {
         var backgroundCanvas = GameObject.FindObjectOfType<BackgroundCanvas>();
         backgroundCanvas.SetImageMaterial(backgroundMaterial);
+        finishLine = GameObject.FindObjectOfType<FinishLine>();
+        var totalStageLength = finishLine.transform.position.y;
+        var checkpointInterval = 0.25f;
+        for (var i = 1; i <= 3; i++) {
+            var checkpointRatio = i * checkpointInterval;
+            var checkpointLine = Instantiate(checkpointPrefab, Vector3.up * totalStageLength * checkpointRatio, Quaternion.identity).GetComponent<CheckpointLine>();
+            checkpointLine.CheckpointText = string.Format("========== {0:F0}% ==========", checkpointRatio * 100);
+        }
     }
 }
