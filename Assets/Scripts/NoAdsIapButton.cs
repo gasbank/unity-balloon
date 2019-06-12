50f91a0bcd9696a74978ce471a06525b63d50cc9
Geoyeob Kim <gasbank@msn.com>
2019-06-12 11:45:23 +0900

    광고 제거 상품 버튼 추가

---
+++ Assets/Scripts/NoAdsIapButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Purchasing;
+
+public class NoAdsIapButton : MonoBehaviour {
+    public void OnPurchaseComplete(Product product) {
+        SushiDebug.Log($"OnPurchaseComplete: {product.definition.id}");
+    }
+
+    public void OnPurchaseFailed(Product product, PurchaseFailureReason reason) {
+        SushiDebug.Log($"OnPurchaseFailed: {product.definition.id}, Reason: ${reason}");
+    }
+}
