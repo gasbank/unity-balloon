cac4b48707c9e66b640ca2c4f7e6fd3584f9b82d
Geoyeob Kim <gasbank@msn.com>
2019-06-12 10:20:40 +0900

    refs #536 스테이지 종료 처리 (광고 연동) - 배너 광고

--- Assets/Scripts/Platform/PlatformAdMobAdsInit.cs
+++ Assets/Scripts/Platform/PlatformAdMobAdsInit.cs
@@ -179,6 +179,21 @@ public class PlatformAdMobAdsInit : MonoBehaviour {
 
             interstitial.Destroy();
         }
+
+        if (bannerView != null) {
+            // Called when an ad request has successfully loaded.
+            bannerView.OnAdLoaded -= HandleOnBannerAdLoaded;
+            // Called when an ad request failed to load.
+            bannerView.OnAdFailedToLoad -= HandleOnBannerAdFailedToLoad;
+            // Called when an ad is clicked.
+            bannerView.OnAdOpening -= HandleOnBannerAdOpened;
+            // Called when the user returned from the app after an ad click.
+            bannerView.OnAdClosed -= HandleOnBannerAdClosed;
+            // Called when the ad click caused the user to leave the application.
+            bannerView.OnAdLeavingApplication -= HandleOnBannerAdLeavingApplication;
+
+            bannerView.Destroy();
+        }
     }
 
     AdRequest DefaultAdRequest => new AdRequest.Builder()
@@ -250,7 +265,52 @@ public class PlatformAdMobAdsInit : MonoBehaviour {
 #endif
 
         // Create a 320x50 banner at the top of the screen.
-        bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
+        bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
+
+        // Called when an ad request has successfully loaded.
+        bannerView.OnAdLoaded += HandleOnBannerAdLoaded;
+        // Called when an ad request failed to load.
+        bannerView.OnAdFailedToLoad += HandleOnBannerAdFailedToLoad;
+        // Called when an ad is clicked.
+        bannerView.OnAdOpening += HandleOnBannerAdOpened;
+        // Called when the user returned from the app after an ad click.
+        bannerView.OnAdClosed += HandleOnBannerAdClosed;
+        // Called when the ad click caused the user to leave the application.
+        bannerView.OnAdLeavingApplication += HandleOnBannerAdLeavingApplication;
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder()
+            .AddTestDevice("F626104A61B1DF52DAFC0B5BE7F72B00") // Galaxy S6 Edge
+            .Build();
+
+        // Load the banner with the request.
+        bannerView.LoadAd(request);
+    }
+
+    public void HandleOnBannerAdLoaded(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLoaded event received");
+    }
+
+    public void HandleOnBannerAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        MonoBehaviour.print("HandleFailedToReceiveAd event received with message: "
+                            + args.Message);
+    }
+
+    public void HandleOnBannerAdOpened(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdOpened event received");
+    }
+
+    public void HandleOnBannerAdClosed(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdClosed event received");
+    }
+
+    public void HandleOnBannerAdLeavingApplication(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLeavingApplication event received");
     }
 #endif // #if GOOGLE_MOBILE_ADS
 }
