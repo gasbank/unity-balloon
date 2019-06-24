9c51aa92e8566837eea23a679dc2db83fd07fa81
Geoyeob Kim <gasbank@msn.com>
2019-06-24 11:42:22 +0900

    Gabe 리소스 3차 적용

---
+++ Assets/Scripts/GearY.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearY : MonoBehaviour {
+    void Update() {
+        //transform.Rotate()
+        transform.Rotate(Vector3.up * Time.deltaTime * 100);
+    }
+}
