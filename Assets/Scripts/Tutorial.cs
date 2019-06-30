817d893a58673fafbc555fcc6f5ee0a6ff0bb6c2
Geoyeob Kim <gasbank@msn.com>
2019-06-30 20:41:31 +0900

    refs #559 피버 게이지를 알려주는 두 번째 스테이지 추가 / 게이브 타이틀 러프 이미지 적용

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -6,7 +6,14 @@ public class Tutorial : MonoBehaviour {
     [SerializeField] CanvasGroup holdToThrustVertically = null;
     [SerializeField] CanvasGroup dragToSteerHorizontally = null;
     [SerializeField] CanvasGroup takeGas = null;
+    [SerializeField] CanvasGroup hitRepulsive = null;
     [SerializeField] HotairBalloon hotairBalloon = null;
+    [SerializeField] int tutorialTemplate = 1;
+
+    public int TutorialTemplate {
+        get => tutorialTemplate;
+        set => tutorialTemplate = value;
+    }
 
     void Start() {
         UpdateReferences();
@@ -18,15 +25,33 @@ public class Tutorial : MonoBehaviour {
     }
 
     void Update() {
-        if (hotairBalloon.IsTitleVisible == false) {
-            if (hotairBalloon.IsGameOver == false) {
-                holdToThrustVertically.alpha = Mathf.Clamp(12.0f - hotairBalloon.Y, 0, 0.8f);
-                dragToSteerHorizontally.alpha = Mathf.Clamp(1.0f - Mathf.Abs((hotairBalloon.Y - 20.0f) / 10.0f), 0, 0.8f);
-                takeGas.alpha = Mathf.Clamp(1.0f - Mathf.Abs((hotairBalloon.Y - 40.0f) / 10.0f), 0, 0.8f);
-            } else {
-                holdToThrustVertically.alpha = 0;
-                dragToSteerHorizontally.alpha = 0;
-                takeGas.alpha = 0;
+        if (TutorialTemplate == 1) {
+            if (hotairBalloon.IsTitleVisible == false) {
+                if (hotairBalloon.IsGameOver == false) {
+                    holdToThrustVertically.alpha = Mathf.Clamp(12.0f - hotairBalloon.Y, 0, 0.8f);
+                    dragToSteerHorizontally.alpha = Mathf.Clamp(1.0f - Mathf.Abs((hotairBalloon.Y - 20.0f) / 10.0f), 0, 0.8f);
+                    takeGas.alpha = Mathf.Clamp(1.0f - Mathf.Abs((hotairBalloon.Y - 40.0f) / 10.0f), 0, 0.8f);
+                    hitRepulsive.alpha = 0;
+                } else {
+                    holdToThrustVertically.alpha = 0;
+                    dragToSteerHorizontally.alpha = 0;
+                    takeGas.alpha = 0;
+                    hitRepulsive.alpha = 0;
+                }
+            }
+        } else {
+            if (hotairBalloon.IsTitleVisible == false) {
+                if (hotairBalloon.IsGameOver == false) {
+                    holdToThrustVertically.alpha = 0;
+                    dragToSteerHorizontally.alpha = 0;
+                    takeGas.alpha = 0;
+                    hitRepulsive.alpha = Mathf.Clamp(12.0f - hotairBalloon.Y, 0, 0.8f);
+                } else {
+                    holdToThrustVertically.alpha = 0;
+                    dragToSteerHorizontally.alpha = 0;
+                    takeGas.alpha = 0;
+                    hitRepulsive.alpha = 0;
+                }
             }
         }
     }
