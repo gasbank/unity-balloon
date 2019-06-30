fbe4c5e4cb075c8bd4867917f99d07519f3b0ef8
Geoyeob Kim <gasbank@gmail.com>
2019-06-30 22:31:05 +0900

    iMac에서 Android 빌드하기 위해 필요한 스크립트 추가

---
+++ build-android.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+UNITY_VERSION=2019.1.8f1
+UNAME_OUT="$(uname -s)"
+case "${UNAME_OUT}" in
+    Linux*)     UNITY_EDITOR="/home/gb/Unity/Hub/Editor/${UNITY_VERSION}/Editor/Unity";;
+    Darwin*)    UNITY_EDITOR="/Applications/Unity/Hub/Editor/${UNITY_VERSION}/Unity.app/Contents/MacOS/Unity";;
+    *)          UNITY_EDITOR="UNKNOWN:${UNAME_OUT}"
+esac
+
+if [ -z "${BUILD_NUMBER}" ]; then
+  BUILD_NUMBER="<NO ENV>"
+fi
+
+${UNITY_EDITOR} \
+    -quit \
+    -batchmode \
+    -executeMethod SushiBuild.PerformAndroidBuild \
+    -logfile >(tee build.log) \
+    -projectPath `pwd` \
+    -buildTarget Android \
+    -keystorePass ${KEYSTORE_PASS} \
+    -buildNumber ${BUILD_NUMBER} \
+    -noGraphics
