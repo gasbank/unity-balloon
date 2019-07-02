a3db016db79c41b027bddd4b148b44df4f49a47c
merk Choi <team.gghf@gmail.com>
2019-07-03 00:00:24 +0900

    1.첫 시작시 로고 나오게 설정 2. 로고 아이콘 바꿈 (거엽님 요청 )

--- Assets/Scripts/StageCommon.cs
+++ Assets/Scripts/StageCommon.cs
@@ -11,11 +11,13 @@ public class StageCommon : MonoBehaviour {
 
     void Awake() {
         // 게임 첫 실행했을 때만 타이틀 이미지 보인다.
-        titleImage.SetActive(awaken == false);
+        //titleImage.SetActive(awaken == false);
         awaken = true;
     }
 
-    public void DeactivateTitleGroup() {
+    public void DeactivateTitleGroup() 
+    {
         titleGroup.SetActive(false);
+        //코드 타이틀 화면이 없어지는 뒤에 퐁하고 벌룬이 나오는 효과를 준다.
     }
 }
