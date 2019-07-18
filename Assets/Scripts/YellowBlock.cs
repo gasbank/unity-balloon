f2dc1218281bcac74047959dbe569c9ad663180e
merk Choi <team.gghf@gmail.com>
2019-07-18 23:52:25 +0900

    Theme 2,3,4 Clouds, some stuff on sky work issue

--- Assets/Scripts/YellowBlock.cs
+++ Assets/Scripts/YellowBlock.cs
@@ -40,7 +40,7 @@ public class YellowBlock : MonoBehaviour {
             hp--;
             hpText.text = hp.ToString();
             player.AddExplosionForce((collision.transform.position - collision.contacts[0].point).normalized);
-            player.IncreaseFeverGauge(10);
+            player.IncreaseFeverGauge(20);
             if (hp <= 0) {
                 if (afterDestroySpawn != null) {
                     if (afterDestroySpawnParent != null) {
