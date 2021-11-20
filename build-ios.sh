8b1b94c08ad7e990f15bce74b01de86608bb193e
Geoyeob Kim <gasbank@gmail.com>
2021-11-20 13:13:27 +0900

    빌드 스크립트

--- build-ios.sh
+++ build-ios.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+UNITY_VERSION=`cat ProjectSettings/ProjectVersion.txt | head -n1 | awk '{print $2;}'`
+UNAME_OUT="$(uname -s)"
+case "${UNAME_OUT}" in
+    Linux*)     UNITY_EDITOR="/home/gb/Unity/Hub/Editor/${UNITY_VERSION}/Editor/Unity";;
+    Darwin*)    UNITY_EDITOR="/Applications/Unity/Hub/Editor/${UNITY_VERSION}/Unity.app/Contents/MacOS/Unity";;
+    *)          UNITY_EDITOR="UNKNOWN:${UNAME_OUT}"
+esac
+
 if [ -z "${BUILD_NUMBER}" ]; then
   BUILD_NUMBER="<NO ENV>"
 fi
 
-/Applications/Unity/Hub/Editor/2019.1.8f1/Unity.app/Contents/MacOS/Unity \
+${UNITY_EDITOR} \
     -quit \
     -batchmode \
     -executeMethod BalloonBuild.PerformIosBuild \
