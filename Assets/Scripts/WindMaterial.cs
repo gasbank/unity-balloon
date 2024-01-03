5e5f7cdc7744de67fd51667ed6bc48dbd6de595e
Geoyeob Kim <gasbank@gmail.com>
2024-01-03 18:51:38 +0900

    WindMaterial 애니메이션 효과 10배 빠르도록

--- Assets/Scripts/WindMaterial.cs
+++ Assets/Scripts/WindMaterial.cs
@@ -15,7 +15,7 @@ public class WindMaterial : MonoBehaviour
 
     void Update()
     {
-        planeRenderer.material.SetFloat("_Center", 1.0f - Mathf.Repeat(Time.time * windRegion.Power / 10, 1.0f));
+        planeRenderer.material.SetFloat("_Center", 1.0f - Mathf.Repeat(Time.time * windRegion.Power, 1.0f));
         planeRenderer.material.SetFloat("_Alpha", windRegion.CapacityRatio);
     }
 }
